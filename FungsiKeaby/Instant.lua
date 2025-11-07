a6903e0a076daf8e8cacc46e2c849541cb1ef5a0
Habibihidayat42 <habibcrash247@gmail.com>
2025-11-07 16:03:36 +0800

    Update Instant.lua

--- FungsiKeaby/Instant.lua
+++ FungsiKeaby/Instant.lua
@@ -20,7 +20,7 @@ local fishing = {
     Settings = {
         FishingDelay = 0.12,
         CancelDelay = 0.05,
-        HookDelay = 0.03,
+        HookDelay = 0.06,
         ChargeToRequestDelay = 0.05,
         FallbackTimeout = 1.5,
     },
