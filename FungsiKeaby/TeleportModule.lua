fd433bdd13b4ea2b2720e00c3c6f0bc9c3038ba6
Habibihidayat42 <habibcrash247@gmail.com>
2025-11-10 14:06:38 +0800

    Update TeleportModule.lua

--- FungsiKeaby/TeleportModule.lua
+++ FungsiKeaby/TeleportModule.lua
@@ -5,7 +5,7 @@ local TeleportModule = {}
 
 TeleportModule.Locations = {
     ["Ancient Jungle"] = Vector3.new(130.70089721679688, 3.5315709114074707, 2769.5888671875),
-    ["Sysiphus Statue"] = Vector3.new(-145.128, 2.992, 3100.453),
+    ["Sysiphus Statue"] = Vector3.new(-3656.56201171875, -134.5314178466797, -964.3167724609375),
     ["Frost Cavern"] = Vector3.new(250.219, 5.014, 3500.002),
     ["Volcanic Rift"] = Vector3.new(-500.011, 4.312, 2600.755)
 }
